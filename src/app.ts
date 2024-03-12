9e03774c9d3f7e4bf7cd7ba8caf2258d4a696083
AnasAAhmed <hafeezroll69@gmail.com>
2024-03-12 15:42:58 +0500

    third commit

--- src/app.ts
+++ src/app.ts
@@ -31,9 +31,7 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors({
-  origin:"http://localhost:5173"
-}));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("API Working with /api/v1");
